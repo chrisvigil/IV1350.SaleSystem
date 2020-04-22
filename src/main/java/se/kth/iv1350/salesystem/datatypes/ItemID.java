8f7145601331083d535706c98b7815393be53554
Chris Vigil <chrisxvigil@gmail.com>
2020-04-22 22:23:26 +0200

    Added ItemID class

---
+++ src/main/java/se/kth/iv1350/salesystem/datatypes/ItemID.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.kth.iv1350.salesystem.datatypes;
+
+/**
+ * Represents a unique itemID
+ * 
+ * @author christopher.vigil
+ */
+public class ItemID {
+    private int id;
+    
+    public ItemID(int id){
+        this.id = id;
+    }
+}
