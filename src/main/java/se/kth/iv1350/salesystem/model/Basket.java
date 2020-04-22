bc3996be42fff5e41856e95fc1df367304c1539b
Chris Vigil <chrisxvigil@gmail.com>
2020-04-22 21:31:32 +0200

    Added basket class

---
+++ src/main/java/se/kth/iv1350/salesystem/model/Basket.java
@@ -0,0 +1,16 @@
+package se.kth.iv1350.salesystem.model;
+
+/**
+ * Collects all items added to sale
+ * 
+ * @author christopher.vigil
+ */
+public class Basket {
+    
+    /**
+     * Creates a new basket
+     */
+    public Basket(){
+        
+    }
+}
