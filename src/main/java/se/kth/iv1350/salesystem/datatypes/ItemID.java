68f605b80cc5168bccf2728ebf7fd0e3bff7762a
Chris Vigil <chrisxvigil@gmail.com>
2020-04-24 10:36:53 +0200

    Implemented ItemID class and testing

--- src/main/java/se/kth/iv1350/salesystem/datatypes/ItemID.java
+++ src/main/java/se/kth/iv1350/salesystem/datatypes/ItemID.java
@@ -5,15 +5,50 @@
  */
 package se.kth.iv1350.salesystem.datatypes;
 
+import java.util.Objects;
+
 /**
  * Represents a unique itemID
  * 
  * @author christopher.vigil
  */
 public class ItemID {
-    private int id;
+    private final String id;
     
     public ItemID(int id){
+        this.id = Integer.toString(id);
+    }
+    
+    public ItemID(String id){
         this.id = id;
+    }
+    
+    public String getID(){
+        return id;
+    }
+    
+    @Override
+    public String toString(){
+        return id;
+    }
+    
+    @Override
+    public boolean equals(Object obj){
+        boolean isEqual = false;
+        if (this.getClass() == obj.getClass())
+        {
+            ItemID itemID = (ItemID) obj;
+            if (itemID.id.equals(this.id))
+                isEqual = true;
+        }
+        
+        return isEqual;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + Objects.hashCode(this.id);
+        return hash;
     }
 }
---
+++ src/test/java/se/kth/iv1350/salesystem/datatypes/ItemIDTest.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.kth.iv1350.salesystem.datatypes;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author christopher.vigil
+ */
+public class ItemIDTest {
+    private final String INIT_STRING = "123456789";
+    private final int INIT_INT = 123456789;
+    private ItemID itemID;
+    
+    @BeforeEach
+    public void setUp() {
+        itemID = new ItemID(INIT_STRING);
+    }
+    
+    @AfterEach
+    public void tearDown() {
+        itemID = null;
+    }
+
+    /**
+     * Test of getID method, of class ItemID.
+     */
+    @Test
+    public void testGetID() {
+        String expected = INIT_STRING;
+        String actual = itemID.getID();
+        assertEquals(expected,actual,"getID does not return expected value");
+    }
+
+    /**
+     * Test of toString method, of class ItemID.
+     */
+    @Test
+    public void testToString() {
+        String expected = INIT_STRING;
+        String actual = itemID.getID();
+        assertEquals(expected,actual,"getID does not return expected value");
+    }
+
+    /**
+     * Test of equals method, of class ItemID.
+     */
+    @Test
+    public void testEquals() {
+        ItemID anotherID = new ItemID(INIT_STRING);
+        boolean isEqual = itemID.equals(anotherID);
+        assertTrue(isEqual, "Two items with the same value are not equal");
+    }
+
+    /**
+     * Test of hashCode method, of class ItemID.
+     */
+    @Disabled("Not implemented")
+    @Test
+    public void testHashCode() {
+        ItemID instance = null;
+        int expResult = 0;
+        int result = instance.hashCode();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+    
+}
