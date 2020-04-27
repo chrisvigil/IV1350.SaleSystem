00d2231b191b20d4b507e6a25b39f72264fc55b8
Chris Vigil <chrisxvigil@gmail.com>
2020-04-27 10:51:14 +0200

    added getters for

--- src/main/java/se/kth/iv1350/salesystem/datatypes/Address.java
+++ src/main/java/se/kth/iv1350/salesystem/datatypes/Address.java
@@ -54,6 +54,26 @@ public class Address {
         this.country = address.country;
     }
     
+    public String getStreet(){
+        return street;
+    }
+    
+    public int getNumber(){
+        return number;
+    }
+    
+    public int getPostalCode(){
+        return postalCode;
+    }
+    
+    public String getCity(){
+        return city;
+    }
+    
+    public String getCountry(){
+        return country;
+    }
+    
     private void setAddress(String street, int number, int postalCode, String city, String country){
         this.street = street;
         this.number = number;
